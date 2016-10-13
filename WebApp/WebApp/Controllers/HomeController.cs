c992c757e7afa92706fd4f7ab8f38b991fae9926
K.D.Rad <davoudi.rk@gmail.com>
2016-10-13 13:36:30 -0700

    added some new changes in the initial code. The new code has a new index named Kian.

--- WebApp/WebApp/Controllers/HomeController.cs
+++ WebApp/WebApp/Controllers/HomeController.cs
@@ -6,13 +6,31 @@ using System.Web.Mvc;
 
 namespace WebApp.Controllers
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class HomeController : Controller
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
             return View();
         }
+        /// <summary>
+        /// This is a test function
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Kian() {
+            return Content("Hello Nurse Next Door");
+        }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -20,6 +38,10 @@ namespace WebApp.Controllers
             return View();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
