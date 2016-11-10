4ec73790e0f33363fa1cdf6b6bc36cc335672738
K.D.Rad <davoudi.rk@gmail.com>
2016-11-10 13:44:23 -0800

    Adding more but not all, Config files

--- bash/reBuild.bat
+++ bash/reBuild.bat
@@ -8,6 +8,8 @@ if exist C:\git\sln\Columbus-Web\NND.CA.Web\bin rmdir /q /s C:\git\sln\Columbus-
 "C:\git\sln\Columbus-Web\.nuget\NuGet.exe" install "C:\git\sln\Columbus-VPF\VPF\packages.config" -source ""  -NonInteractive -RequireConsent -solutionDir "C:\git\sln\ "	
 "C:\git\sln\Columbus-Web\.nuget\NuGet.exe" install "C:\git\sln\Columbus-Framework\Common\Sage.CA.SBS.ERP.Sage300.Common.Utilities\packages.config" -source ""  -NonInteractive -RequireConsent -solutionDir "C:\git\sln\ "	
 "C:\git\sln\Columbus-Web\.nuget\NuGet.exe" install "C:\git\sln\Columbus-Framework\Common\Sage.CA.SBS.ERP.Sage300.Common.Interfaces\packages.config" -source ""  -NonInteractive -RequireConsent -solutionDir "C:\git\sln\ "	
-"C:\git\sln\Columbus-Web\.nuget\NuGet.exe" install "C:\git\sln\Columbus-Framework\Common\Sage.CA.SBS.ERP.Sage300.Common.Interfaces\packages.config" -source ""  -NonInteractive -RequireConsent -solutionDir "C:\git\sln "	
+"C:\git\sln\Columbus-Web\.nuget\NuGet.exe" install "C:\git\sln\Columbus-UnitTest\NND.CA.Web\packages.config" -source ""  -NonInteractive -RequireConsent -solutionDir "C:\git\sln "	
+"C:\git\sln\Columbus-Web\.nuget\NuGet.exe" install "C:\git\sln\Columbus-DV\NND.CA.DV.Services\packages.config" -source ""  -NonInteractive -RequireConsent -solutionDir "C:\git\sln "	
 "C:\git\sln\Columbus-Web\.nuget\NuGet.exe" install "C:\git\sln\Columbus-Web\NND.CA.Web\packages.config" -source ""  -NonInteractive -RequireConsent -solutionDir "C:\git\sln\ "	
+"C:\git\sln\Columbus-Web\.nuget\NuGet.exe" install "C:\git\sln\Columbus-Framework\Common\NND.CA.Common.Model\packages.config" -source ""  -NonInteractive -RequireConsent -solutionDir "C:\git\sln\ "	
 
