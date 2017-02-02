f9057bc9dc6b1f26ba6476018ab1f0f5aa7b83f3
Kian <davoudi.rk@gmail.com>
2017-02-02 14:13:18 -0800

    adding Kentico into the Test Project. This is an MVC project. Its is clean. I have not yet complied it. Only added Kentico.web.mvc into the nugets.

---
+++ Kentico/kenticoTestSln/kenticoTestSln/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(kenticoTestSln.Startup))]
+namespace kenticoTestSln
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
