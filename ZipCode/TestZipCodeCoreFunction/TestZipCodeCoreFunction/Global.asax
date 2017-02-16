c45c3eeb6cea3709466aba85a4382f59c7dde63f
Kian <davoudi.rk@gmail.com>
2017-02-16 13:02:07 -0800

    fresh init

---
+++ ZipCode/TestZipCodeCoreFunction/TestZipCodeCoreFunction/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestZipCodeCoreFunction.MvcApplication" Language="C#" %>
---
+++ ZipCode/TestZipCodeCoreFunction/TestZipCodeCoreFunction/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestZipCodeCoreFunction.Startup))]
+namespace TestZipCodeCoreFunction
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
